6830b60971cf0dc4c222a3c0e5f0f6ed19cce48c
tomixrm <chocolate0785lego@icloud.com>
2024-04-20 00:45:26 +0900

    ✨タイマー割り込み1KHzでHeartBeatするようにした
    生きてるって感じ！！

--- 2024/STMDev/F446RE_cubeide/src/app/app.cpp
+++ 2024/STMDev/F446RE_cubeide/src/app/app.cpp
@@ -6,12 +6,13 @@
 #include "i2c.h"
 #include "BNO055.hpp"
 #include "DigitalInOut.hpp"
-
+#include "PWMSingle.hpp"
 
 CANBus can(&hcan1, 0x100);
 DigitalOut led0(LED0_GPIO_Port, LED0_Pin);
 DigitalOut led1(LED1_GPIO_Port, LED1_Pin);
 DigitalOut led2(LED2_GPIO_Port, LED2_Pin);
+PwmSingleOut ledH(&htim1, TIM_CHANNEL_1);
 
 CANBus::CANData canRecvData = {
     .stdId = 0x555,
@@ -20,6 +21,7 @@ CANBus::CANData canRecvData = {
 
 BNO055 bno(&hi2c1);
 Timer timer;
+int i = 0;
 
 void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan) {
     if (can.getHcan() == &hcan1) {
@@ -27,6 +29,15 @@ void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan) {
         canRecvData.stdId = 0x555;
         can.send(canRecvData);
         led0 = !led0;
+    }
+}
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+    if (htim == &htim10) {
+        // タイマー10をタイマー割り込みに使用する
+        // 1msごとに呼び出される
+        i++;
+        ledH.write(MyMath::sinDeg(int(i / 10)) / 2 + 0.5); // Heartbeat
     }
 }
 
@@ -41,6 +52,7 @@ void setup() {
     bno.accConfig();
     bno.init();
     can.init();
+    ledH.init();
 }
 
 void main_app() {
@@ -50,6 +62,6 @@ void main_app() {
         led1 = !led1;
         led2 = !led2;
         printfDMA("Hello\n");
-        HAL_Delay(100);
+        HAL_Delay(500);
     }
 }
