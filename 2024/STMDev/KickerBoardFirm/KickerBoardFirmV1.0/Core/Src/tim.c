430f5cd860043767dda3c9e50fc907dac6346f31
ochiishi <ce0073hx@ed.ritsumei.ac.jp>
2024-04-24 20:22:51 +0900

    kickerfirm1.0

--- 2024/STMDev/KickerBoardFirm/KickerBoardFirmV1.0/Core/Src/tim.c
+++ 2024/STMDev/KickerBoardFirm/KickerBoardFirmV1.0/Core/Src/tim.c
@@ -108,9 +108,9 @@ void MX_TIM3_Init(void)
 
   /* USER CODE END TIM3_Init 1 */
   htim3.Instance = TIM3;
-  htim3.Init.Prescaler = 0;
+  htim3.Init.Prescaler = 63;
   htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim3.Init.Period = 65535;
+  htim3.Init.Period = 9999;
   htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_PWM_Init(&htim3) != HAL_OK)
@@ -153,9 +153,9 @@ void MX_TIM15_Init(void)
 
   /* USER CODE END TIM15_Init 1 */
   htim15.Instance = TIM15;
-  htim15.Init.Prescaler = 0;
+  htim15.Init.Prescaler = 63;
   htim15.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim15.Init.Period = 65535;
+  htim15.Init.Period = 9999;
   htim15.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim15.Init.RepetitionCounter = 0;
   htim15.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
--- 2024/STMDev/KickerBoardFirm/KickerBoardFirmV1.0/src/app/app.cpp
+++ 2024/STMDev/KickerBoardFirm/KickerBoardFirmV1.0/src/app/app.cpp
@@ -1,15 +1,48 @@
 #include "app.h"
 #include "DigitalInOut.hpp"
+#include "PWMSingle.hpp"
+#include "adc.h"
+#include "usart.h"
+#include <cstdio>
+#include <cstring>
+
 int data = 0;
-DigitalOut led0(LED0_GPIO_Port, LED0_Pin);
+DigitalOut debugled(DEBUG_LED_GPIO_Port, DEBUG_LED_Pin);
+DigitalOut canled(CAN_LED_GPIO_Port, CAN_LED_Pin);
+DigitalOut charge(CHARGE_GPIO_Port, CHARGE_Pin);
+PwmSingleOut straightkicker(&htim15, TIM_CHANNEL_2);
+PwmSingleOut chipkicker(&htim3, TIM_CHANNEL_2);
+PwmSingleOut dribbler(&htim1, TIM_CHANNEL_2);
+
+uint16_t adc_Value = 0;
 
 void setup() {
+    straightkicker.init();
+    chipkicker.init();
+    dribbler.init();
 }
 
 void main_app() {
     setup();
+    printf("starttt\n\r");
+    uint8_t msg[] = "hello world!\n\r";
     while (1) {
-        led0 = !led0;
+        HAL_ADC_Start(&hadc1);
+        HAL_ADC_PollForConversion(&hadc1, 1000);
+        adc_Value = HAL_ADC_GetValue(&hadc1);
+        printf("adc_Value = %d", adc_Value);
+        HAL_UART_Transmit(&huart1, (uint8_t *)adc_Value, 1, 100);
+        HAL_Delay(500);
+
+        HAL_UART_Transmit(&huart1, msg, strlen((char *)msg), 100);
+        HAL_Delay(500);
+
+        straightkicker.write(0.2);
+        chipkicker.write(0.3);
+        dribbler.write(0.5);
+        charge = 0;
+        debugled = !debugled;
+        canled = !canled;
         HAL_Delay(500);
         data++;
     }
