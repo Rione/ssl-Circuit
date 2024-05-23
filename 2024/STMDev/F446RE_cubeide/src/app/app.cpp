dcf4c49f62889528d6713e0403d5e4d90e78d313
tomixrm <chocolate0785lego@icloud.com>
2024-05-23 18:48:56 +0900

    なぞ
    いけると思ったけどなんかうごかん

--- 2024/STMDev/F446RE_cubeide/src/app/app.cpp
+++ 2024/STMDev/F446RE_cubeide/src/app/app.cpp
@@ -9,7 +9,7 @@ CANBus::CANData canRecvData;
 
 MainMode mainMode('M', "Main Mode", &robot);
 
-MPU9250 imu(&hspi2, SPI2_CS0_GPIO_Port, SPI2_CS0_Pin);
+MPU9250 mpu(&hspi2, SPI2_CS0_GPIO_Port, SPI2_CS0_Pin);
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
     if (htim == &htim10) {
@@ -35,11 +35,29 @@ void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan) {
 
 void setup() {
     robot.hardwareInit();
+
+    mpu.setGyroFullScaleRange(GFSR_500DPS);
+    mpu.setAccFullScaleRange(AFSR_4G);
+    mpu.setDeltaTime(0.004);
+    mpu.setTau(0.98);
+
+    if (mpu.begin() != true) {
+        printf("MPU9250 initialization failed\n");
+        while (1) {
+            robot.led0 = !robot.led0;
+            HAL_Delay(100);
+        }
+    }
+
+    printf("CALIBRATING\n");
+    mpu.calibrateGyro(1500);
 }
 
 void main_app() {
     setup();
     while (1) {
+        RawData imu = mpu.readRawData();
+        printf("x: %d, y: %d, z: %d\n", imu.ax, imu.ay, imu.az);
         // mainMode.loop();
     }
 }
