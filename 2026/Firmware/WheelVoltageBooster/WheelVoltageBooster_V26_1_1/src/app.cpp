431d2669242198a8a72438c0a171160e07f2a48b
Tsukigake <kamiokannde829@gmail.com>
2025-07-26 14:36:57 +0900

    gpio.cは存在しないそうですまる

--- 2026/Firmware/WheelVoltageBooster/WheelVoltageBooster_V26_1_1/src/app.cpp
+++ 2026/Firmware/WheelVoltageBooster/WheelVoltageBooster_V26_1_1/src/app.cpp
@@ -6,5 +6,14 @@ void Setup(void){
 
 void MainLoop(){
   HAL_GPIO_WritePin(LED_1_GPIO_Port,LED_1_Pin,GPIO_PIN_SET);
+  HAL_GPIO_WritePin(LED_2_GPIO_Port,LED_2_Pin,GPIO_PIN_SET);
+  //HAL_GPIO_WritePin(LED_3_GPIO_Port,LED_3_Pin,GPIO_PIN_SET);
+  HAL_GPIO_WritePin(LED_CAN_GPIO_Port,LED_CAN_Pin,GPIO_PIN_SET);
+  HAL_Delay(1000);
+  HAL_GPIO_WritePin(LED_1_GPIO_Port,LED_1_Pin,GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(LED_2_GPIO_Port,LED_2_Pin,GPIO_PIN_RESET);
+  //HAL_GPIO_WritePin(LED_3_GPIO_Port,LED_3_Pin,GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(LED_CAN_GPIO_Port,LED_CAN_Pin,GPIO_PIN_RESET);
+  HAL_Delay(1000);
 }
 
