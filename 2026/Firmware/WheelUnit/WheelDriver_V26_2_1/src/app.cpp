18fc103f0c408781d1bc265ca0c14b92d21cedd3
月駆 <82303471+Tsukigake@users.noreply.github.com>
2025-12-10 14:57:37 +0900

    time_ms設定

--- 2026/Firmware/WheelUnit/WheelDriver_V26_2_1/src/app.cpp
+++ 2026/Firmware/WheelUnit/WheelDriver_V26_2_1/src/app.cpp
@@ -1,23 +1,35 @@
 #include "app.hpp"
 
 void Setup(void){
-  
+  HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(WAKE_GPIO_Port, WAKE_Pin, GPIO_PIN_SET);
+  HAL_Delay(200);
 }
 
 void MainLoop(){
-  HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_SET);
-  HAL_GPIO_WritePin(WAKE_GPIO_Port, WAKE_Pin, GPIO_PIN_SET);
-  HAL_Delay(100);
-  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 1000);
+  int time_ms = 5;
+  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 500);
   __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, 0);
   __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_3, 0);
-  HAL_Delay(100);
-  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 0);
-  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, 1000);
+  HAL_Delay(time_ms);
+  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 500);
+  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, 500);
   __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_3, 0);
-  HAL_Delay(100);
+  HAL_Delay(time_ms);
+  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 0);
+  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, 500);
+  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_3, 0);
+  HAL_Delay(time_ms);
+  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 0);
+  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, 500);
+  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_3, 500);
+  HAL_Delay(time_ms);
   __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 0);
   __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, 0);
-  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_3, 1000);
-  HAL_Delay(100);
+  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_3, 500);
+  HAL_Delay(time_ms);
+  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 500);
+  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, 0);
+  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_3, 500);
+  HAL_Delay(time_ms);
 }
