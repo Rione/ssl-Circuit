b096a8f1b78274cd582adcccb71df3ccc706b46d
banba takumi <96656737+banbatakumi@users.noreply.github.com>
2025-12-24 10:18:46 +0900

    make通った

---
+++ 2026/Firmware/Dribbler/Dribbler_V26_1_1/src/app.c
@@ -0,0 +1,16 @@
+#include "app.h"
+
+PwmOut LED1;
+PwmOut LED2;
+PwmOut LED3;
+PwmOut LED4;
+DigitalIn SW;
+
+// Serial pc;
+
+void Setup() {
+      printf("Dribbler Setup Start\n");
+}
+
+void MainApp() {
+}
