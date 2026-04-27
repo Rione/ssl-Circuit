7ee7e48164134488bd07bd90610e6422f2306941
banba takumi <96656737+banbatakumi@users.noreply.github.com>
2026-04-28 02:27:48 +0900

    MXピン設定半分くらい完了

--- 2026/Firmware/WheelUnit/WheelDriver_V26_3/Core/Inc/main.h
+++ 2026/Firmware/WheelUnit/WheelDriver_V26_3/Core/Inc/main.h
@@ -57,6 +57,20 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define LED1_Pin GPIO_PIN_1
+#define LED1_GPIO_Port GPIOC
+#define LED2_Pin GPIO_PIN_2
+#define LED2_GPIO_Port GPIOC
+#define LED3_Pin GPIO_PIN_3
+#define LED3_GPIO_Port GPIOC
+#define nFAULT_Pin GPIO_PIN_15
+#define nFAULT_GPIO_Port GPIOE
+#define LEDB_Pin GPIO_PIN_5
+#define LEDB_GPIO_Port GPIOB
+#define LEDG_Pin GPIO_PIN_6
+#define LEDG_GPIO_Port GPIOB
+#define LEDR_Pin GPIO_PIN_7
+#define LEDR_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
--- 2026/Firmware/WheelUnit/WheelDriver_V26_3/Core/Src/main.c
+++ 2026/Firmware/WheelUnit/WheelDriver_V26_3/Core/Src/main.c
@@ -18,6 +18,10 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "opamp.h"
+#include "tim.h"
+#include "usart.h"
+#include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -84,6 +88,15 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_OPAMP1_Init();
+  MX_OPAMP2_Init();
+  MX_OPAMP3_Init();
+  MX_TIM1_Init();
+  MX_TIM3_Init();
+  MX_TIM4_Init();
+  MX_USART1_UART_Init();
+  MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
